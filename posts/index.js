afad9361afa88081ebabe7d7908feca9d7bd937f
Alaka Adam Olasumbo <sumbomatic@gmail.com>
2021-06-23 10:16:42 +0100

    first commit

---
+++ posts/index.js
@@ -0,0 +1,35 @@
+const cors = require('cors')
+const dotenv = require('dotenv')
+const path = require('path')
+const {randomBytes} = require('crypto')
+const bodyparser = require('body-parser')
+const express = require('express')
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 8080
+app.use(bodyparser.json())
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const posts = {};
+ 
+ app.get('/posts',(req, res )=> {
+    res.send(posts);
+ });
+
+  app.post('/posts',(req, res )=> {
+    const id = randomBytes(4).toString('hex')
+    const {title} = req.body
+
+    posts[id] = {
+        id, title
+    }
+    res.status(201).send(posts[id]);
+  })
+  
+const server = app.listen(port, function () {
+  console.log(`listening on port ${port}`)
+})
+
+module.exports = server
